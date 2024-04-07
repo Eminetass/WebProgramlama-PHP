bcad5cafedd646a3b65c78153eee3bfa0d6c7578
Emine Tas <eminetas126@gmail.com>
2024-04-08 02:30:29 +0300

    dort islem

---
+++ dortIslem.php
@@ -0,0 +1,14 @@
+<?php
+  $sayi1=25; 
+  $sayi2=5;
+
+  $toplama = $sayi1+$sayi2; //Toplama
+  $cikarma = $sayi1-$sayi2; //Çıkarma
+  $carpma = $sayi1*$sayi2; //Çarpma
+  $bolme = $sayi1/$sayi2; //Bölme
+
+  echo "Toplama = ".$toplama."<br>";
+  echo "Çıkarma = ".$cikarma."<br>"; 
+  echo "Çarpma = ".$carpma."<br>"; 
+  echo "Bölme = ".$bolme; 
+?>
