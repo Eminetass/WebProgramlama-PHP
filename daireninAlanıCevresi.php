8ea66a86e54f0a5b03b2ec52ed7a2c64597da3fe
Emine Tas <eminetas126@gmail.com>
2024-04-09 03:43:17 +0300

    dairenin alanı-cevresini hesaplama

---
+++ daireninAlanıCevresi.php
@@ -0,0 +1,12 @@
+<?php
+   $daireYaricap=5;
+   $daireAlan;
+   $daireCevre;
+   
+   $daireAlan =3.14 * $daireYaricap * $daireYaricap;
+   echo("Daire alanı: ".$daireAlan."<br>");
+   
+   $daireCevre = 2*3.14*$daireYaricap;
+   echo("Daire çevresi: ".$daireCevre);
+   
+?>
