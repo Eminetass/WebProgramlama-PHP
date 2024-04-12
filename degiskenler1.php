58d5b71b333cf0edf72bed3eaf1cff6cb3134ec0
Emine Tas <eminetas126@gmail.com>
2024-04-13 00:20:20 +0300

    degiskenler

---
+++ degiskenler1.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<?php
+    $var_bool = TRUE;
+	$var_int = 21;
+	$var_float = 217.234;
+	$var_str1 = "PHP veri türleri";
+	$var_str2 = 'PHP değişkenler';
+	
+	echo "boolean değer: $var_bool </br>";
+	echo "integer değer: $var_int</br>";
+	echo "float değer: $var_float </br>";
+	echo "karakter dizisi: $var_str1 </br>";
+	echo "karakter dizisi: $var_str2 </br>";
+?>
+</body>
+</html>
