a17310ef1f415ce4af023180fdfc5a021962e5c3
Emine Tas <eminetas126@gmail.com>
2024-05-09 15:59:36 +0300

    dereceyi fahrenheit'e çevirme

---
+++ fahrenheit.php
@@ -0,0 +1,9 @@
+<?php 
+//kullanıcıdan alınan dereceyi fahrenheit'e dönüştüren programın Php dilinde kodu
+   $derece = 37.0;
+   $fahrenheit;
+   
+   $fahrenheit = ($derece * 1.8) + 32;
+   print($derece." derecenin fahrenheit karşılığı: ".$fahrenheit);
+
+?>
